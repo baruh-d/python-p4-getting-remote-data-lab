8eb0c2bc8c5b046972a768e24a5436f9cd61a06d
baruh-d <barudaniel6@gmail.com>
2024-04-07 14:37:13 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if not response.ok:
+            print("Failed to retrieve data.")
+        else:
+            return response.content
 
     def load_json(self):
-        pass
+        try:
+            body = self.get_response_body()
+            return json.loads(body)
+        except ValueError as e:
+            print("Couldn't parse JSON:", e)
+            
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+requester = GetRequester(url)
+data = requester.load_json()
+print(data)
